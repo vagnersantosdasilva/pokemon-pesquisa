ccd2ecf764dea519ca8b988d383f734b00a57fad
vagnersantosdasilva <vagnersantosdasilva@gmail.com>
2024-09-21 20:45:17 -0300

    pegando retorno de service renderizando o resultado no  card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BuscaComponent } from './components/busca/busca.component';
+import { CardComponent } from './components/card/card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,BuscaComponent],
+  imports: [RouterOutlet,BuscaComponent, CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'pokemon-pesquisa';
+   // Declarando as propriedades pokemonList e pokemonDetails
+   pokemonList: any[] = [];
+  
+   pokemonDetails: any = null;
+   handleSearchResults(event: any) {
+     if (event.type === 'list') {
+       this.pokemonList = event.data;
+       this.pokemonDetails = null;
+     } else if (event.type === 'details') {
+       this.pokemonDetails = event.data;
+       this.pokemonList = [];
+     }
+   }
 }
--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PokemonService } from '../../service/pokemon.service';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
 export class CardComponent {
 
+  constructor(private pokemonService: PokemonService) { }
+
+  @Input() pokemonList: any[] = [];
+  @Input() pokemonDetails: any = null;
+
+  ngOnInit(): void {
+    // Optional: If fetchPokemonDetails is called before data is received
+    if (!this.pokemonDetails) {
+      this.fetchPokemonDetails(this.pokemonList[0].name); // Use the first Pokemon by default
+    }
+  }
+
+  fetchPokemonDetails(name: string) {
+    this.pokemonService.getPokemon(name)
+      .subscribe(response => {
+        const { sprites, name, types } = response; // Destructuring for clarity
+        const pokemonDetails = {
+          sprite: sprites.front_default, // Extract front sprite
+          name,
+          type: types[0].type.name // Assuming primary type
+        };
+        this.pokemonDetails = pokemonDetails;
+        console.log(pokemonDetails);
+      });
+  }
+
+  imageLoaded = false;  // Controle de carregamento da imagem
+
+  // Método chamado quando a imagem é carregada
+  onImageLoad() {
+    this.imageLoaded = true;
+  }
 }
