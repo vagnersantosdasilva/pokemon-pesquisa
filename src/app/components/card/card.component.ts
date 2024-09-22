bdf8595876758fa67e2b4b0e2a52907c170bc15d
vagnersantosdasilva <vagnersantosdasilva@gmail.com>
2024-09-22 15:19:45 -0300

    corrigindo carregamento e pokemon em lista

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -15,15 +15,15 @@ export class CardComponent {
 
   @Input() pokemonList: any[] = [];
   @Input() pokemonDetails: any = null;
-  @Input() pokemonSelect: any = null;
+  @Input() pokemonSelect: any = {name:'',sprites:'',abilities:[],base_experience:'',weight:'',height:'',stats:[]};
   pokemonSelectedList: any[] = [];
 
-  // ngOnInit(): void {
-  //   // Optional: If fetchPokemonDetails is called before data is received
-  //   if (!this.pokemonDetails) {
-  //     this.fetchPokemonDetails(this.pokemonList[0].name); // Use the first Pokemon by default
-  //   }
-  // }
+  ngOnInit(): void {
+    // Optional: If fetchPokemonDetails is called before data is received
+    if (!this.pokemonDetails) {
+      this.fetchPokemonDetails(this.pokemonList[0].name); // Use the first Pokemon by default
+    }
+  }
 
   fetchPokemonDetails(name: string) {
     this.pokemonService.getPokemon(name)
@@ -34,11 +34,32 @@ export class CardComponent {
       });
   }
 
+  // fetchPokemonSelect(pokemonName: string, index: number) {
+  //   this.pokemonService.getPokemon(pokemonName).subscribe((data) => {
+  //     this.pokemonSelect = data;
+  //     this.pokemonSelectedList[index] = { ...data, imageLoaded: false }; // Reiniciar estado da imagem
+  //     this.cdr.detectChanges(); // Forçar a atualização do template
+  //   });
+  // }
+
   fetchPokemonSelect(pokemonName: string, index: number) {
     this.pokemonService.getPokemon(pokemonName).subscribe((data) => {
-      this.pokemonSelect = data;
-      this.pokemonSelectedList[index] = { ...data, imageLoaded: false }; // Reiniciar estado da imagem
-      this.cdr.detectChanges(); // Forçar a atualização do template
+      if (!this.pokemonSelectedList[index]) {
+        // Se o pokemon não estiver na lista, inicializa o objeto e reseta o estado de carregamento
+        this.pokemonSelectedList[index] = { ...data, imageLoaded: false }; 
+      } else {
+        // Se o pokemon já está na lista, mantém o estado de imageLoaded
+        this.pokemonSelectedList[index] = { ...data, imageLoaded: this.pokemonSelectedList[index].imageLoaded };
+      }
+      this.cdr.detectChanges(); // Atualiza o template
+      setTimeout(() => {
+        // Forçar a abertura do modal manualmente (caso necessário)
+        const modalTrigger = document.querySelector('#pokemonModal') as HTMLElement;
+        if (modalTrigger) {
+          modalTrigger.click(); // Simula o clique no modal para abri-lo
+        }
+      }, 100); // Pequeno atraso para sincronização
+    
     });
   }
 
@@ -48,7 +69,7 @@ export class CardComponent {
     }
   }
 
-
+ 
   imageLoaded = false;  // Controle de carregamento da imagem
 
   //Método chamado quando a imagem é carregada
